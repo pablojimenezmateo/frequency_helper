c68fbc07d8343eec0004536c1b0a302eed9dfacb
Pablo Jiménez Mateo <gef3233@gmail.com>
2013-05-25 14:05:41 +0300

    Initial commit, old code.

---
+++ README.md
@@ -0,0 +1,28 @@
+Frequency helper
+=================
+
+A simple tool to help with cryptology frequency search.
+
+Usage:
+------
+
+You give the program a string that will follow a regular expresion to find all similar words.
+
+Use "_" to match any word, "#" to match same character inside the word and any other character to match.
+
+Example:
+-----
+
+We have the word 'ashhe'
+I guess the two 'h' represent the same character, I know it has length 5 and that there is the same characte in the 3rd and 4th positions.
+The string to match such words would be '__##_'.
+
+Disclaimer:
+-----
+
+This is a really old program when I was first startin to code. It is even coded in spanish (Although I have changed the outputs) so don't expect good code from it.
+
+License:
+-------
+
+This program is licensed under Creative commons Attribution 3.0 Unported, more info : http://creativecommons.org/licenses/by/3.0/deed.en_US
---
+++ frequency_helper.py
@@ -0,0 +1,89 @@
+opcion=False
+while not opcion:
+	print 'Choose a dictionary'
+	print '1.- Spanish'
+	print '2.- English'
+	choice=int(raw_input('Option: '))
+	if choice==1:
+		print 'All the special characters have been removed'
+		f=open("Dictionaries/spanish.txt","r")
+		opcion=True
+	elif choice==2:
+		f=open("Dictionaries/english.txt","r")
+		opcion=True
+print "\nUse '#' to multiple matches in the same word"
+print "\nUse '_' for any other character"
+print "\nUse a letter to match it in that place"
+print "\nExample: __##a# would return zurrar\n"
+dato=raw_input("Write a word: ")
+longitud=len(dato)
+
+#Sacamos las posiciones de las letras claves
+repetidos=[]
+datos=[]
+palabras=[]
+for i in range(0,longitud):
+	if(dato[i]=='#'):
+		repetidos.append(i)
+	elif(dato[i]!='_'):
+		datos.append(i)
+
+#Hacemos las comprobaciones
+for palabra in f:
+	if(len(palabra)==(longitud+1)):
+		candidato=1
+		if len(repetidos)!=0:
+			pos=repetidos[0]
+			for j in repetidos:
+				if(palabra[j]!=palabra[pos]):
+					candidato=0
+					break
+		if(candidato==1):
+			for k in datos:
+				if(palabra[k]!=dato[k]):
+					candidato=0
+					break
+			if(candidato==1):
+				print palabra
+				palabras.append(palabra)
+
+sub=raw_input('Do a subsearch? (Y/N): ')
+while sub != 'Y' and sub != 'N':
+	sub=raw_input('Do a subsearch? (Y/N): ')
+while sub=='Y':
+	dato=raw_input("Write a word: ")
+	longitud=len(dato)
+	repetidos=[]
+	datos=[]
+	temp=[]
+	for i in range(0,longitud):
+	        if(dato[i]=='#'):
+	                repetidos.append(i)
+	        elif(dato[i]!='_'):
+	                datos.append(i)
+
+	#Hacemos las comprobaciones
+	for palabra in palabras:
+		#El +1 es por el '\n'
+	        if(len(palabra)==(longitud+1)):
+	                candidato=1
+	                if len(repetidos)!=0:
+	                        pos=repetidos[0]
+	                        for j in repetidos:
+	                                if(palabra[j]!=palabra[pos]):
+	                                        candidato=0
+	                                        break   
+	                if(candidato==1):
+	                        for k in datos:
+	                                if(palabra[k]!=dato[k]):
+	                                        candidato=0
+	                                        break
+	                        if(candidato==1):
+	                                print palabra
+	                                temp.append(palabra)
+	palabras=temp
+	sub=raw_input('Do a subsearch? (Y/N): ')
+	while sub != 'Y' and sub != 'N':
+        	sub=raw_input('Do a subsearch? (Y/N): ')
+
+f.close()
